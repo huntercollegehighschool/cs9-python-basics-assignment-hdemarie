f8e8c655089c71408f2bc83b6b0d060ddcf580b4
basil <103700377+hdemarie@users.noreply.github.com>
2022-04-15 00:05:13 +0000

    part 2:
    set variables so the code can run the multiplication / addition through those instead of through the printed text

--- part2.py
+++ part2.py
@@ -3,13 +3,14 @@ ______
 PART 2
 ______
 
-Run the code below. Fix it so that the program will print the product of the number 
-and 10 in the formatted sentence that's already there, and the sum of 99 and the 
-number in the formatted sentence that's already there.
+Run the code below. Fix it so that the program will print the product of the number and 10 in the formatted sentence that's already there, and the sum of 99 and the number in the formatted sentence that's already there.
 
 '''
 
 #code starts here
-number = input("Enter a number: ")
-print("Ten times your number is", 10 * number)
-print("Ninety-nine more than your number is", 99 + number)
+number = int(input("Enter a number: "))
+a = 10 * number
+b = 99 + number
+
+print("Ten times your number is", a)
+print("Ninety-nine more than your number is", b)
